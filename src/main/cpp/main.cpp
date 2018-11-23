9a40bc132d60b8932d4f945c53dd7122b790546a
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-11-23 11:53:16 -0700

    initialize opengl functions

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "greeting.hpp"
 
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+
+using namespace std;
+
+void display(void)  {
+    return;
+}
+
 int main(int argc, char *argv[])    {
     std::cout << greeting << std::endl;
+
+    // setup OpenGL
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("Box");
+    glutDisplayFunc(display);
+    glutMainLoop();
+
     return 0;
 }
